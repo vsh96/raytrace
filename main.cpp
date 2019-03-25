873196ccfa5b99f48f7daf9876e0122e92e11e46
Vlad Hrabovskiy <hrabovskiyvlad@gmail.com>
2019-03-25 13:01:46 +0200

    Added dielectric material

--- main.cpp
+++ main.cpp
@@ -55,12 +55,13 @@ int main()
     vec3 vert(0.0f, 2.0f, 0.0f);
     vec3 origin(0.0f, 0.0f, 0.0f);
 
-    body *list[4];
+    body *list[5];
     list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
     list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100.0, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-    list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.8, 0.8), 9.0));
-    body *world = new scene(list, 4);
+    list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1.0, 0.0, -1.0), -0.45, new dielectric(1.5));
+    body *world = new scene(list, 5);
     camera cam;
 
     for( int j = IMG_HEIGHT-1; j>=0; j-- )
--- material.hpp
+++ material.hpp
@@ -26,6 +26,27 @@ vec3 reflect(const vec3& v, const vec3& n)
     return v - n*dot(v,n)*2;
 }
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
+{
+    vec3 uv = normalize(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
+    if(discriminant > 0)
+    {
+        refracted = (uv-n*dt)*ni_over_nt - n*sqrt(discriminant);
+        return true;
+    }
+    else
+        return false;
+}
+
+float schlick(float cosine, float ref_idx)
+{
+    float r0 = (1-ref_idx)/(1+ref_idx);
+    r0=r0*r0;
+    return r0+(1-r0)*pow((1-cosine), 5);
+}
+
 class material
 {
 public:
@@ -57,4 +78,51 @@ public:
     }
     float roughness;
     vec3 albedo;
+};
+
+class dielectric : public material {
+    public:
+        dielectric(float ri) : ref_idx(ri) {}
+        virtual bool scatter(const ray in, const hit& h, vec3& atten, ray& ref) const 
+        {
+            vec3 outward_normal;
+            vec3 reflected = reflect(in.direction, h.normal);
+            float ni_over_nt;
+            atten = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+            float reflect_prob;
+            float cosine;
+            if(dot(in.direction, h.normal) > 0)
+            {
+                outward_normal = -h.normal;
+                ni_over_nt = ref_idx;
+                cosine = ref_idx*dot(in.direction, h.normal)/length(in.direction);
+            }
+            else
+            {
+                outward_normal = h.normal;
+                ni_over_nt = 1.0/ref_idx;
+                cosine = -dot(in.direction, h.normal)/length(in.direction);
+            }
+            if(refract(in.direction, outward_normal, ni_over_nt, refracted))
+            {
+                reflect_prob = schlick(cosine, ref_idx);
+            }
+            else
+            {
+                ref = ray(h.p, reflected);
+                reflect_prob = 1.0;
+            }
+
+            if(drand48() < reflect_prob)
+            {
+                ref = ray(h.p, reflected);
+            }
+            else
+            {
+                ref = ray(h.p, refracted);
+            }
+            return true;
+        }
+        float ref_idx;
 };
